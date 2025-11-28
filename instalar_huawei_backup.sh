d0158685ee9f30f1b442c60c4183d7ce0056ac3c
Br10Consultoria <125372525+Br10Consultoria@users.noreply.github.com>
2025-11-28 10:58:33 -0300

    Update instalar_huawei_backup.sh

--- instalar_huawei_backup.sh
+++ instalar_huawei_backup.sh
@@ -99,6 +99,25 @@ services:
     tty: true
 EOF
 
+cat << 'EOF' > entrypoint.sh
+#!/bin/bash
+
+touch /var/log/cron.log
+chmod 666 /var/log/cron.log
+
+service rsyslog start
+service cron start
+
+echo "=== Serviços Iniciados ==="
+echo "Cron e rsyslog estão rodando."
+echo "Monitorando /var/log/cron.log..."
+echo "==========================="
+
+tail -f /var/log/cron.log
+EOF
+
+chmod +x entrypoint.sh
+
 
 # requirements.txt
 cat << 'EOF' > requirements.txt
